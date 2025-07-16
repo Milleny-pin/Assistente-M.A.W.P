c4699e11ce261ca08f5cc6ab9c18f6b4e43e8736
Milleny Souza Braghine <Millenybragh@gmail.com>
2025-07-15 22:31:27 -0300

    Implementando HTML e JS para o chat

---
+++ static/js.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const Imagebot = document.getElementById('Imagebot');
+    const chatOverlay = document.getElementById('chatOverlay');
+    const closeChatBtn = document.getElementById('closeChatBtn');
+    const messageForm = document.getElementById('messageForm');
+    const userinput = document.getElementById('userInput');
+    const chatMessages = document.getElementById('chatMessages');
+    const sendMessageBtn = document.getElementById('sendMessageBtn');
+    const loadingIndicator = document.getElementById('loadingIndicator');
+    const messageContainer = document.getElementById('messageContainer');
+
+})
